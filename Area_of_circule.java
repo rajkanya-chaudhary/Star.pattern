df77fcd775b17eb46bc0b896e4628d048b8abcbc
rajkanya-chaudhary <dc5068786@gmail.com>
2024-04-26 23:41:13 +0530

    this is a circulr program using radius formula

---
+++ Area_of_circule.java
@@ -0,0 +1,15 @@
+public class Area_of_circule
+ {
+    public static void main(String[] args) 
+    {
+    double radius = 7;			
+		
+    //formula for area of a circle
+    double area = 3.14 * radius * radius;		
+    System.out.println(area);
+    
+}
+}
+
+
+
