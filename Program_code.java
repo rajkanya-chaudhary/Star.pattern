303f872e73b3bd5534d3467f32ef75d38f2c5c2c
rajkanya-chaudhary <dc5068786@gmail.com>
2024-04-30 23:43:50 +0530

    this is a table program using for loop

---
+++ Program_code.java
@@ -0,0 +1,9 @@
+public class Program_code {
+    public static void main(String[] args) {
+        System.out.println("Table print");
+        int n = 2;
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(n + " * " + i + " = " + (n * i));
+        }
+    }
+}
